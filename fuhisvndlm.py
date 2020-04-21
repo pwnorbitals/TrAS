9c8e787368f11a8118338b5c773bc7e8958cc776
MagixInTheAir <c.de-claverie@pm.me>
2020-04-21 15:08:43 +0200

    make graphs work

--- fuhisvndlm.py
+++ fuhisvndlm.py
@@ -15,7 +15,32 @@ from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
 from matplotlib.figure import Figure
 
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.signal import find_peaks
+from scipy.stats import linregress
+from datetime import datetime
+from matplotlib.widgets import Slider
+from math import floor
+
+jiulian = __import__("jiulian")
+
 progname = os.path.basename(sys.argv[0])
+
+
+def getVal(i, arr) :
+    if i <= 0:
+        return arr[0]
+    if i >= len(arr):
+        return arr[-1]
+    else:
+        return arr[i]
+
+
+def smooth(y, box_pts):
+    box = np.ones(box_pts)/box_pts
+    y_smooth = np.convolve(y, box, mode='same')
+    return y_smooth
 
 
 
@@ -26,18 +51,6 @@ class data(object):
 		self.processDataX = processDataX
 		self.processDataY = processDataY
 		self.repertory = repertory
-    
-
-	def csvRead(self):
-		pass
-
-	def lissage(self):
-    #moving average + taff willy
-		pass
-
-	def truc_De_Chris(self):
-	#traitement des données
-		pass
 
 
 
@@ -50,8 +63,6 @@ class Canvas(FigureCanvas):
         self.axes = fig.add_subplot(111)
         super(Canvas, self).__init__(fig)
 
-        self.compute_initial_figure()
-
         FigureCanvas.__init__(self, fig)
         self.setParent(parent)
 
@@ -62,43 +73,41 @@ class Canvas(FigureCanvas):
 
 
 
+"""
 
-class RawDataCanvas(Canvas,data):
-    """Simple canvas with a sine plot."""
-    
+class DataCanvas(Canvas,data):    
     def compute_initial_figure(self):
         t = arange(0.0, 3.0, 0.01)
         s = sin(2*pi*t)
         self.axes.cla()
         self.axes.plot(t, s)
-        self.axes.set_title('Raw data')
+        self.axes.set_title('Data')
     
     def compute_figure(self, a):
         t = arange(0.0, 3.0, 0.01)
         s = sin(2*pi*t*a)
         self.axes.clear()
         self.axes.plot(t, s)
-        self.axes.set_title('Raw data')
+        self.axes.set_title('Data')
         self.fig.canvas.draw()
         self.fig.canvas.flush_events()
 
-class ProcessDataCanvas(Canvas,data):
-
+class ErrorsCanvas(Canvas,data):
     def compute_initial_figure(self):
         t = arange(0.0, 3.0, 0.01)
         s = sin(2*pi*t)
         self.axes.plot(t, s)
-        self.axes.set_title('Process data')
+        self.axes.set_title('Standard errors')
 
     def compute_figure(self, k):
         t = arange(0.0, 3.0, 0.01)
         s = sin(2*pi*t*k)
         self.axes.clear()
         self.axes.plot(t, s)
-        self.axes.set_title('Process data')
+        self.axes.set_title('Standard errors')
         self.fig.canvas.draw()
         self.fig.canvas.flush_events()
-
+"""
 
 class ApplicationWindow(QtWidgets.QMainWindow):
     def __init__(self):
@@ -128,25 +137,135 @@ class ApplicationWindow(QtWidgets.QMainWindow):
         grid.addWidget(self.GroupGraph(), 0,0)
         grid.addWidget(self.GroupResult(),0,1)
         grid.setSpacing(10)
+
+        self.k = 8
+        self.a = 0
+        self.b = 0
         
 
     def OpenFile(self):
         file = QFileDialog.getOpenFileName(self, 'Open File')
         if file[0]:
-            f = open(file[0], 'r')
+            # Parse file
+            file = open(file[0], 'r')
+            lines = file.readlines()
+            file.close()
+            self.lines = [line[:-1].split(' ') for line in lines]
+            self.parseData()
 
-            with f:
-                data = f.read()
+    def parseData(self):
+        lines = self.lines
+        header = lines[0]
+        meta = lines[1]
+        data = np.array([x for x in lines[2:] if x[header.index("V-C")] != "99.99999"]).T
 
-        print(data)
+        JDHEL = data[header.index("JDHEL")]
+        VC = data[header.index("V-C")]
+        VC = [-1 * float(d) for d in VC]
+        #HELCOR= data[header.index("HELCOR")]
+        dates = [jiulian.from_jd(float(mjd), fmt='jd') for mjd in JDHEL]
+        timestamps = [datetime.timestamp(i) for i in dates]
+
+        #print([date.strftime("%b %d %Y %H:%M:%S") for date in dates])
+        #print(VC)
+        self.dataCanvas.axes.clear()
+        self.dataCanvas.axes.set_title("Data")
+        self.dataCanvas.axes.plot(timestamps, [float(d) for d in VC])
+        
+
+
+        # Compute resolution 
+        timediff = max(dates) - min(dates)
+        timediff_min = timediff.total_seconds() / 60
+        print("Time difference : ", timediff, "(", timediff_min, " minutes)")
+
+        resolution = timediff_min / len(VC)
+        print("Resolution : ", resolution, " samples per minute")
+
+
+        # Compute standard errors (use tunable parameter)
+        nb_vals = int(self.k * resolution)
+        errors = []
+        for i in range(len(VC)):
+            #min_max = (i - nb_vals, i + nb_vals)
+            vals = [float(getVal(i + j, VC)) for j in range(-nb_vals, +nb_vals)]
+            #print(vals)
+            avg = np.average(vals)
+            sn = np.sqrt((1/(len(vals)-1)) * sum([(xi - avg)**2 for xi in vals]) )  # Standard deviation
+            #print(med, sn)
+            errors.append(sn / np.sqrt(len(vals)))
+
+            #print(errors)
+        errors_smoothed = smooth(errors, nb_vals)
+        self.errorCanvas.axes.clear()
+        self.errorCanvas.axes.set_title('Standard errors')
+        self.errorCanvas.axes.plot(timestamps, errors_smoothed)
+
+        # Find local maxima
+        peaks, properties = find_peaks(errors_smoothed, prominence=1e-8)
+        print("PEAKS : ", peaks)
+        print("PEAKS Y : ", errors_smoothed[peaks])
+
+        prominences = properties["prominences"]
+        #print("PROMINENCES : ", prominences)
+
+        sort_arr = [[i, peaks[i], prominences[i]] for i in range(len(peaks))]
+
+        sorted_kept_peaks = sorted(sort_arr, key=lambda entry: entry[2], reverse=True)[:6]
+        kept_peaks = [elem[1] for elem in sorted_kept_peaks]
+
+        for i in range(len(kept_peaks)):
+            self.errorCanvas.axes.axvline(x=timestamps[kept_peaks[i]], color='red')
+
+        # Fit between SE maxima (linear regression)
+        # https://exoplanetarchive.ipac.caltech.edu/docs/datasethelp/AXA.html
+        # http://brucegary.net/AXA/Fitting/fitting.html
+        fits = []
+        kept_peaks.insert(0, 0)
+        kept_peaks = sorted(kept_peaks)
+        print("KEPT PEAKS : ", kept_peaks)
+        
+
+        for i in range(len(kept_peaks)):
+
+            # subdivide interval
+            min_of_range = kept_peaks[i]
+
+            if i+1 == len(kept_peaks):
+                max_of_range = len(timestamps)
+            else:
+                max_of_range = kept_peaks[i+1]
+            #print(min_of_range, max_of_range)
+
+            # get x and y data for linear regression on the interval
+            x = timestamps[min_of_range:max_of_range]
+            y = VC[min_of_range:max_of_range]
+
+            # run linear regression
+            fits.append(linregress(x,y)) # slope, intercept, r_value, p_value, std_err
+
+            # get x and y value after regression
+            xvals = timestamps[min_of_range:max_of_range]
+            yvals = [fits[-1].slope * timestamps[j] + fits[-1].intercept for j in range(min_of_range, max_of_range)]
+            #self.dataCanvas.axes.plot(y, xvals)
+            self.dataCanvas.axes.plot(xvals, yvals)
+            #print(fits)
+
+        self.errorCanvas.fig.canvas.draw()
+        self.errorCanvas.fig.canvas.flush_events()
+        self.dataCanvas.fig.canvas.draw()
+        self.dataCanvas.fig.canvas.flush_events()
+
+
+            # Deduce parameters
 
 
     def GroupGraph(self):
          #graphics and toolbars        
-        Rd = RawDataCanvas(self.main_widget, width=5, height=5, dpi=100)
-        self.rd = Rd
-        Cd = ProcessDataCanvas(self.main_widget, width=5, height=5, dpi=100)
-        self.cd = Cd
+        Rd = Canvas(self.main_widget, width=5, height=5, dpi=100)
+        self.dataCanvas = Rd
+        Cd = Canvas(self.main_widget, width=5, height=5, dpi=100)
+        self.errorCanvas = Cd
         toolbarRd = NavigationToolbar(Rd, self)
         toolbarCd = NavigationToolbar(Cd, self)
 
@@ -177,6 +296,7 @@ class ApplicationWindow(QtWidgets.QMainWindow):
         sliderK.setTickPosition(QSlider.TicksBothSides)
         sliderK.setTickInterval(10)
         sliderK.setSingleStep(1)
+        sliderK.setValue(self.k)
         sliderK.valueChanged.connect(self.changeK)
         self.sk = sliderK
 
@@ -185,8 +305,18 @@ class ApplicationWindow(QtWidgets.QMainWindow):
         sliderA.setTickPosition(QSlider.TicksBothSides)
         sliderA.setTickInterval(10)
         sliderA.setSingleStep(1)
+        sliderA.setValue(self.a)
         sliderA.valueChanged.connect(self.changeA)
         self.sa = sliderA
+
+        sliderB = QSlider(Qt.Horizontal)
+        sliderB.setFocusPolicy(Qt.StrongFocus)
+        sliderB.setTickPosition(QSlider.TicksBothSides)
+        sliderB.setTickInterval(10)
+        sliderB.setSingleStep(1)
+        sliderB.setValue(self.b)
+        sliderB.valueChanged.connect(self.changeB)
+        self.sb = sliderB
 
         groupBoxResult = QGroupBox('Results')
         vbox = QVBoxLayout()
@@ -195,6 +325,7 @@ class ApplicationWindow(QtWidgets.QMainWindow):
         vbox.addWidget(sliderK)
         vbox.addWidget(labelInfosA)
         vbox.addWidget(sliderA)
+        vbox.addWidget(sliderB)
         vbox.addStretch(1)
         groupBoxResult.setLayout(vbox)       
         
@@ -205,11 +336,20 @@ class ApplicationWindow(QtWidgets.QMainWindow):
         self.close()
 
 
-    def changeA(self, a_in):
-        self.rd.compute_figure(self.sa.value())
+    def changeA(self):
+        self.a = self.sa.value()
+        self.compute_figures()
 
-    def changeK(self, k_in):
-        self.cd.compute_figure(self.sk.value())
+    def changeB(self):
+        self.b = self.sb.value()
+        self.compute_figures()
+
+    def changeK(self):
+        self.k = self.sk.value()
+        self.compute_figures()
+
+    def compute_figures(self):
+        pass
 
 qApp = QtWidgets.QApplication(sys.argv)
 
