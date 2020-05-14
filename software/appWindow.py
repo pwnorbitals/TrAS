afef2fe28406f3c83feab7fd90dd851cd260a02f
MagixInTheAir <c.de-claverie@pm.me>
2020-05-14 11:50:57 +0200

    MORE REFACTORING

--- software/appWindow.py
+++ software/appWindow.py
@@ -9,13 +9,11 @@ import math
 import threading
 
 # PyQT
-from PyQt5 import QtWidgets
 from PyQt5.QtCore import Qt
+from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import (QLabel, QLineEdit, 
     QGridLayout, QGroupBox, QVBoxLayout, QSlider, QFileDialog, 
     QComboBox, QPushButton, QMessageBox)
-from PyQt5.QtGui import QDoubleValidator
-
 
 
 # Matplotlib
@@ -23,14 +21,12 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
 
-
-
 # Local files
 import aboutDialog
 import settingsDialog
 import canvas
 import parseData
-
+import groupResult
 
 # Settings
 matplotlib.use('Qt5Agg')
@@ -83,7 +79,7 @@ class ApplicationWindow(QtWidgets.QMainWindow):
 
         grid = QGridLayout(self.main_widget)
         grid.addWidget(self.GroupGraph(), 0,0)
-        grid.addLayout(self.GroupResult(),0,1)
+        grid.addLayout(groupResult.GroupResult(self),0,1)
         grid.addWidget(self.GroupDataBase(),0,2)
         grid.setSpacing(10)
 
@@ -139,141 +135,6 @@ class ApplicationWindow(QtWidgets.QMainWindow):
         groupBoxGraph.setLayout(vbox)       
         
         return groupBoxGraph
-        
-    def GroupResult(self):
-        # Menu deroulant
-        self.MenuS = QComboBox(self)
-        self.MenuS.textActivated.connect(self.RefStarChanged)
-        
-        labelMenuS = QtWidgets.QLabel()
-        labelMenuS.setText("Reference Star")
-
-        labelRadius = QtWidgets.QLabel()
-        labelRadius.setText("Enter the Star's radius (solar radius):")
-
-        labelPeriod = QtWidgets.QLabel()
-        labelPeriod.setText("Enter the Period of orbit (days):")
-
-        self.PlanetLabel = QLabel('Planet radius : \nPlanet mass : ')
-        self.StarLabel = QLabel('Star density : \nStar mass : ')
-        self.Other = QLabel('Impact parameter b : \nSemimajor axis a : \nInclinaison : ')
-        self.LC = QLabel('Depth : \nTotal duration : \nFull duration : ')
-
-        self.labelInfosK = QLabel('Change the K coefficient :  (note: coeff was calculated to be optimal)')
-
-        self.labelInfosS = QLabel('Change the s coefficient : ')
-
-        labelInfosA = QtWidgets.QLabel()
-        labelInfosA.setText('Change boundary values from left and right :')
-
-        tuto1 = "The K coefficient characterize the time's interval used for error calculation.\nIncrease K implies less approximation segments."
-        tuto2 = "\n\nThe s coefficient is the number of samples used to smooth the data.\n\n\n"
-        labeltuto = QLabel(tuto1+tuto2)
-
-        self.labelInfoP = QtWidgets.QLabel()
-        self.labelInfoP.setText('The prominence value changes the sensitivity of peak detection on the errors graph : ')
-
-        self.validator = QDoubleValidator()
-        self.RS_input = QLineEdit()
-        self.RS_input.setValidator(self.validator)
-        self.RS_input.returnPressed.connect(self.RadiusChanged)
-        
-        self.Per_input = QLineEdit()
-        self.Per_input.setValidator(self.validator)
-        self.Per_input.returnPressed.connect(self.PeriodChanged)
-
-        sliderS = QSlider(Qt.Horizontal)
-        sliderS.setFocusPolicy(Qt.StrongFocus)
-        sliderS.setTickPosition(QSlider.TicksBothSides)
-        sliderS.setTickInterval(10)
-        sliderS.setSingleStep(1)
-        sliderS.setMinimum(1)
-        sliderS.valueChanged.connect(self.compute_figures)
-        self.ss = sliderS
-        sliderS.setValue(2)
-
-        sliderK = QSlider(Qt.Horizontal)
-        sliderK.setFocusPolicy(Qt.StrongFocus)
-        sliderK.setTickPosition(QSlider.TicksBothSides)
-        sliderK.setTickInterval(10)
-        sliderK.setSingleStep(1)
-        sliderK.setMinimum(2)
-        sliderK.valueChanged.connect(self.compute_figures)
-        self.sk = sliderK
-        sliderK.setValue(8)
-
-        sliderP = QSlider(Qt.Horizontal)
-        sliderP.setFocusPolicy(Qt.StrongFocus)
-        sliderP.setTickPosition(QSlider.TicksBothSides)
-        sliderP.setTickInterval(10)
-        sliderP.setSingleStep(1)
-        sliderP.valueChanged.connect(self.compute_figures)
-        self.sp = sliderP
-        sliderP.setValue(8)
-
-        sliderA = QSlider(Qt.Horizontal)
-        sliderA.setFocusPolicy(Qt.StrongFocus)
-        sliderA.setTickPosition(QSlider.TicksBothSides)
-        sliderA.setTickInterval(10)
-        sliderA.setSingleStep(1)
-        sliderA.valueChanged.connect(self.compute_figures)
-        self.sa = sliderA
-        sliderA.setValue(0)
-
-        sliderB = QSlider(Qt.Horizontal)
-        sliderB.setFocusPolicy(Qt.StrongFocus)
-        sliderB.setTickPosition(QSlider.TicksBothSides)
-        sliderB.setTickInterval(10)
-        sliderB.setSingleStep(1)
-        sliderB.valueChanged.connect(self.compute_figures)
-        sliderB.setInvertedAppearance(True)
-        self.sb = sliderB
-        sliderB.setValue(0)
-
-        hbox = QtWidgets.QHBoxLayout()
-        hbox.addWidget(sliderA)
-        hbox.addWidget(sliderB)
-
-        GridResult = QGridLayout()
-        #User Input
-        GridResult.addWidget(labelRadius, 0,0)
-        GridResult.addWidget(self.RS_input, 1,0)
-        GridResult.addWidget(labelPeriod, 0,1)
-        GridResult.addWidget(self.Per_input, 1,1)
-        
-        #Deduced Parameter
-        GridResult.addWidget(self.PlanetLabel, 2,0)
-        GridResult.addWidget(self.StarLabel, 3,0)
-        GridResult.addWidget(self.LC, 2,1)
-        GridResult.addWidget(self.Other, 3,1)
-        
-        groupBoxCalculations = QGroupBox('Calculations')
-        calbox = QVBoxLayout()
-        calbox.addLayout(GridResult)
-        calbox.addStretch(1)
-        groupBoxCalculations.setLayout(calbox)
-        
-        groupBoxResult = QGroupBox('Settings')
-        vbox = QVBoxLayout()
-        vbox.addWidget(labelMenuS)
-        vbox.addWidget(self.MenuS)
-        vbox.addWidget(self.labelInfosK)
-        vbox.addWidget(sliderK)
-        vbox.addWidget(self.labelInfosS)
-        vbox.addWidget(sliderS)
-        vbox.addWidget(self.labelInfoP)
-        vbox.addWidget(sliderP)
-        vbox.addWidget(labelInfosA)
-        vbox.addLayout(hbox)
-        vbox.addWidget(labeltuto)
-        vbox.addStretch(1)
-        groupBoxResult.setLayout(vbox)       
-
-        ret = QVBoxLayout()
-        ret.addWidget(groupBoxResult)
-        ret.addWidget(groupBoxCalculations)
-        
-        return ret
     
     def GroupDataBase(self):
         labelName = QtWidgets.QLabel()
---
+++ software/groupResult.py
@@ -0,0 +1,139 @@
+from PyQt5.QtWidgets import QComboBox, QLabel, QSlider, QLineEdit, QHBoxLayout, QGridLayout, QGroupBox, QVBoxLayout
+from PyQt5.QtGui import QDoubleValidator
+from PyQt5.QtCore import Qt
+from PyQt5 import QtWidgets
+
+def GroupResult(self):
+        # Menu deroulant
+        self.MenuS = QComboBox(self)
+        self.MenuS.textActivated.connect(self.RefStarChanged)
+        
+        labelMenuS = QLabel()
+        labelMenuS.setText("Reference Star")
+
+        labelRadius = QLabel()
+        labelRadius.setText("Enter the Star's radius (solar radius):")
+
+        labelPeriod = QLabel()
+        labelPeriod.setText("Enter the Period of orbit (days):")
+
+        self.PlanetLabel = QLabel('Planet radius : \nPlanet mass : ')
+        self.StarLabel = QLabel('Star density : \nStar mass : ')
+        self.Other = QLabel('Impact parameter b : \nSemimajor axis a : \nInclinaison : ')
+        self.LC = QLabel('Depth : \nTotal duration : \nFull duration : ')
+
+        self.labelInfosK = QLabel('Change the K coefficient :  (note: coeff was calculated to be optimal)')
+
+        self.labelInfosS = QLabel('Change the s coefficient : ')
+
+        labelInfosA = QLabel()
+        labelInfosA.setText('Change boundary values from left and right :')
+
+        tuto1 = "The K coefficient characterize the time's interval used for error calculation.\nIncrease K implies less approximation segments."
+        tuto2 = "\n\nThe s coefficient is the number of samples used to smooth the data.\n\n\n"
+        labeltuto = QLabel(tuto1+tuto2)
+
+        self.labelInfoP = QLabel()
+        self.labelInfoP.setText('The prominence value changes the sensitivity of peak detection on the errors graph : ')
+
+        self.validator = QDoubleValidator()
+        self.RS_input = QLineEdit()
+        self.RS_input.setValidator(self.validator)
+        self.RS_input.returnPressed.connect(self.RadiusChanged)
+        
+        self.Per_input = QLineEdit()
+        self.Per_input.setValidator(self.validator)
+        self.Per_input.returnPressed.connect(self.PeriodChanged)
+
+        sliderS = QSlider(Qt.Horizontal)
+        sliderS.setFocusPolicy(Qt.StrongFocus)
+        sliderS.setTickPosition(QSlider.TicksBothSides)
+        sliderS.setTickInterval(10)
+        sliderS.setSingleStep(1)
+        sliderS.setMinimum(1)
+        sliderS.valueChanged.connect(self.compute_figures)
+        self.ss = sliderS
+        sliderS.setValue(2)
+
+        sliderK = QSlider(Qt.Horizontal)
+        sliderK.setFocusPolicy(Qt.StrongFocus)
+        sliderK.setTickPosition(QSlider.TicksBothSides)
+        sliderK.setTickInterval(10)
+        sliderK.setSingleStep(1)
+        sliderK.setMinimum(2)
+        sliderK.valueChanged.connect(self.compute_figures)
+        self.sk = sliderK
+        sliderK.setValue(8)
+
+        sliderP = QSlider(Qt.Horizontal)
+        sliderP.setFocusPolicy(Qt.StrongFocus)
+        sliderP.setTickPosition(QSlider.TicksBothSides)
+        sliderP.setTickInterval(10)
+        sliderP.setSingleStep(1)
+        sliderP.valueChanged.connect(self.compute_figures)
+        self.sp = sliderP
+        sliderP.setValue(8)
+
+        sliderA = QSlider(Qt.Horizontal)
+        sliderA.setFocusPolicy(Qt.StrongFocus)
+        sliderA.setTickPosition(QSlider.TicksBothSides)
+        sliderA.setTickInterval(10)
+        sliderA.setSingleStep(1)
+        sliderA.valueChanged.connect(self.compute_figures)
+        self.sa = sliderA
+        sliderA.setValue(0)
+
+        sliderB = QSlider(Qt.Horizontal)
+        sliderB.setFocusPolicy(Qt.StrongFocus)
+        sliderB.setTickPosition(QSlider.TicksBothSides)
+        sliderB.setTickInterval(10)
+        sliderB.setSingleStep(1)
+        sliderB.valueChanged.connect(self.compute_figures)
+        sliderB.setInvertedAppearance(True)
+        self.sb = sliderB
+        sliderB.setValue(0)
+
+        hbox = QHBoxLayout()
+        hbox.addWidget(sliderA)
+        hbox.addWidget(sliderB)
+
+        GridResult = QGridLayout()
+        #User Input
+        GridResult.addWidget(labelRadius, 0,0)
+        GridResult.addWidget(self.RS_input, 1,0)
+        GridResult.addWidget(labelPeriod, 0,1)
+        GridResult.addWidget(self.Per_input, 1,1)
+        
+        #Deduced Parameter
+        GridResult.addWidget(self.PlanetLabel, 2,0)
+        GridResult.addWidget(self.StarLabel, 3,0)
+        GridResult.addWidget(self.LC, 2,1)
+        GridResult.addWidget(self.Other, 3,1)
+        
+        groupBoxCalculations = QGroupBox('Calculations')
+        calbox = QVBoxLayout()
+        calbox.addLayout(GridResult)
+        calbox.addStretch(1)
+        groupBoxCalculations.setLayout(calbox)
+        
+        groupBoxResult = QGroupBox('Settings')
+        vbox = QVBoxLayout()
+        vbox.addWidget(labelMenuS)
+        vbox.addWidget(self.MenuS)
+        vbox.addWidget(self.labelInfosK)
+        vbox.addWidget(sliderK)
+        vbox.addWidget(self.labelInfosS)
+        vbox.addWidget(sliderS)
+        vbox.addWidget(self.labelInfoP)
+        vbox.addWidget(sliderP)
+        vbox.addWidget(labelInfosA)
+        vbox.addLayout(hbox)
+        vbox.addWidget(labeltuto)
+        vbox.addStretch(1)
+        groupBoxResult.setLayout(vbox)       
+
+        ret = QVBoxLayout()
+        ret.addWidget(groupBoxResult)
+        ret.addWidget(groupBoxCalculations)
+        
+        return ret
