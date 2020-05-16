7d8007f5b4599ec6ee6aa1c43bacab27b585618f
MagixInTheAir <c.de-claverie@pm.me>
2020-05-16 14:57:25 +0200

    add logo on the interface

--- software/python/main.py
+++ software/python/main.py
@@ -6,15 +6,12 @@ import sys
 import appWindow
 
 scriptDir = os.path.dirname(os.path.realpath(__file__))
-scriptDir = scriptDir.replace('software'+os.path.sep+'python','Img')
-
-
 progname = os.path.basename(sys.argv[0])
 
 qApp = QtWidgets.QApplication(sys.argv)
 
 aw = appWindow.ApplicationWindow()
 aw.setWindowTitle("%s" % progname)
-aw.setWindowIcon(QIcon(scriptDir + os.path.sep + 'LOGO5.png'))
+aw.setWindowIcon(QIcon(scriptDir + os.path.sep + '..' + os.path.sep + 'img' + os.path.sep + 'LOGO5.png'))
 aw.show()
 sys.exit(qApp.exec_())
